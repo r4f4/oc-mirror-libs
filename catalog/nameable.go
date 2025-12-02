b01cff1026b6b31e1a5040557d05e67ef3f2764f
Rafael Fonseca <r4f4rfs@gmail.com>
2025-12-02 10:31:19 +0100

    catalog: export the nameable interface

--- catalog/nameable.go
+++ catalog/nameable.go
@@ -5,29 +5,29 @@ import (
 	"iter"
 )
 
-type nameable interface {
+type Nameable interface {
 	GetName() string
 }
 
 // CompareByName compares two `nameable` instances
-func CompareByName[T nameable](a T, b T) int {
+func CompareByName[T Nameable](a T, b T) int {
 	return cmp.Compare(a.GetName(), b.GetName())
 }
 
 // EqualByName returns a comparator to check if `T` has `name`.
-func EqualByName[T nameable](name string) func(e T) bool {
+func EqualByName[T Nameable](name string) func(e T) bool {
 	return func(e T) bool {
 		return e.GetName() == name
 	}
 }
 
 // CompareToName compares `T`'s name with `name`.
-func CompareToName[T nameable](e T, name string) int {
+func CompareToName[T Nameable](e T, name string) int {
 	return cmp.Compare(e.GetName(), name)
 }
 
 // Names returns an iterator for all the names in the sequence `seq`.
-func Names[T nameable](seq iter.Seq[T]) iter.Seq[string] {
+func Names[T Nameable](seq iter.Seq[T]) iter.Seq[string] {
 	return func(yield func(string) bool) {
 		for v := range seq {
 			if !yield(v.GetName()) {
